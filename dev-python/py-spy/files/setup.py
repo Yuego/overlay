5e31b688919884cf9100fe9ce4c690b22e89f95c
layman <layman@localhost>
2021-06-08 23:41:14 +0400

    upd

--- dev-python/py-spy/files/setup.py
+++ dev-python/py-spy/files/setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="py-spy",
     version="0.3.7",
-    rust_extensions=[RustExtension("py_spy", binding=Binding.PyO3)],
+    rust_extensions=[RustExtension("py_spy", binding=Binding.Exec)],
     packages=["py_spy"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
